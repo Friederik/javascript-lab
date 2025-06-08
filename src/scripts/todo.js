9e5dfcf5d2226d960a46812e4444dd0ce950cdda
Friederik <70342668+Friederik@users.noreply.github.com>
2025-06-09 04:25:08 +0500

    todo #3
    Done setup

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -1,13 +1,113 @@
-const btn = document.getElementById('saveBtn')
-const input = document.getElementById('noteInput')
-const output = document.getElementById('output')
-
-const buffer = JSON.parse(localStorage.getItem('name'))
-if (buffer) {
-    output.textContent = buffer
+function saveLocal(newTasks) {
+    localStorage.setItem('task', JSON.stringify(newTasks))
 }
 
-btn.addEventListener('click', () => {
-    localStorage.setItem('name', JSON.stringify(input.value))
-    output.textContent = input.value 
-})
+function getLocal() {
+    const local = localStorage.getItem('task')
+    
+    if (local) return JSON.parse(local)
+    else return []
+}
+
+function renderTask(task) {
+    const list = document.getElementById('list')
+
+    const article = document.createElement('article')
+    const h2 = document.createElement('h2')
+    const div = document.createElement('div')
+    const btnDone = document.createElement('button')
+    const btnDelete = document.createElement('button')
+
+
+    article.id = `t-${task.index}`
+    article.classList.add('task')
+
+    h2.classList.add('task__header')
+    h2.textContent = task.name
+
+    div.classList.add('task__btns')
+
+    btnDone.classList.add('task__btn', task.isDone ? 'task__btn--released' : 'task__btn--release')
+    btnDone.textContent = task.isDone ? 'Отменить' : 'Выполнить'
+    btnDelete.classList.add('task__btn', 'task__btn--delete')
+    btnDelete.textContent = 'Удалить'
+
+    btnDone.addEventListener('click', () => {
+        if (btnDone.classList.contains('task__btn--released')){
+            btnDone.classList.remove('task__btn--released')
+            btnDone.classList.add('task__btn--release')
+
+            btnDone.textContent = 'Выполнить'
+        } else {
+            btnDone.classList.remove('task__btn--release')
+            btnDone.classList.add('task__btn--released')
+
+            btnDone.textContent = 'Отменить'
+        }
+        changeTask(task.index)
+    })
+
+    btnDelete.addEventListener('click', () => {
+        article.remove()
+        deleteTask(task.index)
+    })
+
+    div.append(btnDone, btnDelete)
+    article.append(h2, div)
+    
+    list.prepend(article)
+}
+
+function renderList() {
+    const list = document.getElementById('list')
+    list.innerHTML = ''
+    if (getLocal().length === 0) {
+        const noTask = document.createElement('article')
+        noTask.classList.add('task')
+        noTask.classList.add('task--no-children')
+        noTask.classList.add('task__header')
+        noTask.textContent = "Добавьте задачи!"
+        list.appendChild(noTask)
+    } else {
+        for (let task of getLocal()) {
+            renderTask(task)
+        }
+    }
+
+}
+
+function addTask() {
+    const input = document.getElementById('input')
+    
+    if (input.value.trim() !== '') {
+        const local = getLocal()
+        local.push({
+            name: input.value.trim(),
+            index: Date.now(),
+            isDone: false
+        })
+        saveLocal(local)
+        renderList()
+        input.value = ''
+    }
+}
+
+function deleteTask(index) {
+    const local = getLocal()
+    const newLocal = local.filter(task => task.index !== index)
+    saveLocal(newLocal)
+}
+
+function changeTask(index) {
+    const local = getLocal()
+    const task = local.find(task => task.index === index)
+    if (task) {
+        task.isDone = !task.isDone  
+        saveLocal(local)
+    }
+    
+}
+
+
+document.getElementById('add-btn').addEventListener('click', addTask)
+renderList()
