2b6ca75e2b53d6cdfa4a06aeac757fe2e9738ee6
Friederik <70342668+Friederik@users.noreply.github.com>
2025-06-15 15:35:02 +0500

    First True Callback

---
+++ src/scripts/async.js
@@ -0,0 +1,15 @@
+function fisrtCounter(time) {
+    // const timeOut = time
+
+    setTimeout(() => console.log(`Консольный лог ${time} секунд`), time * 1000)
+}
+
+// counter(2)
+// counter(1)
+// counter(4)
+
+// function recurtion(stamp, newFunc) {
+//     return setTimeout(stamp => newFunc(stamp), 2000)
+// }
+
+// recurtion
